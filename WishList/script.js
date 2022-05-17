c0183f0f581abe16a58a113cacab6fd7449c378c
KonstantinAleksandrov <constantine22811@gmail.com>
2022-05-17 17:08:52 +0300

    wishList Home Worck

--- WishList/script.js
+++ WishList/script.js
@@ -2,7 +2,7 @@ fetch('products.json')
   .then(res => res.json())
   .then(json => renderProducts(json))
 
-let wishList = []
+/* let wishList = []
 let fullAmount = 0
 
 const renderWishList = (wishList) => {
@@ -45,4 +45,122 @@ const renderProducts = (productList) => {
     item.append(img, button, name, price)
     listContainer.append(item)
   })
+} */
+
+let wishList = []
+let allprice = 0
+
+const renderProducts = (gameList)=>{
+  const listContainer = document.querySelector('.list-container')
+
+  Object.values(gameList).forEach((game)=>{
+    const item = document.createElement('div')
+    item.classList.add('item')
+
+    const imgContainer = document.createElement('div')
+    imgContainer.classList.add('img-container')
+
+    const img = document.createElement('img')
+    img.src = game.cover
+
+    const button = document.createElement('div')
+    button.classList.add('button')
+    button.textContent = 'Добавить'
+    button.addEventListener('click',(event)=>addGameInWishList(event,game))
+
+    const name = document.createElement('div')
+    name.classList.add('name')
+    name.textContent = game.name
+
+    const price = document.createElement('div')
+    price.classList.add('price')
+    price.textContent = `${game.price} руб.`
+
+    imgContainer.append(img)
+    item.append(imgContainer)
+    item.append(button)
+    item.append(name)
+    item.append(price)
+    listContainer.append(item)
+  })
 }
+
+const addGameInWishList = (event,game) =>{
+  if(wishList.includes(game)){
+    wishList = wishList.filter((f)=> f.name !== game.name)
+    event.target.textContent = 'Добавить'
+    event.target.style.color = 'rgb(0,206,255)'
+  }else{
+    wishList = [...wishList,game]
+    event.target.textContent = 'В списке'
+    event.target.style.color = 'rgb(8,194,119)'
+  }
+  
+  createCardListItem()
+}
+
+
+
+const createCardListItem = () =>{
+  const cardList = document.querySelector('.card-list')
+  cardList.innerHTML = ''
+  wishList.forEach((item)=>{
+    const li = document.createElement('li')
+    li.textContent = item.name
+    cardList.append(li)
+
+    const closeCross = document.createElement('div')
+    closeCross.classList.add('closeCross')
+    closeCross.addEventListener('click',(()=>deleteCardListItem(item)))
+
+    const firsSpan = document.createElement('span')
+    const secondSpan = document.createElement('span')
+
+    li.append(closeCross)
+    closeCross.append(firsSpan)
+    closeCross.append(secondSpan)
+  })
+  allprice = wishList.reduce((sum,item)=>{
+    if(!item.price){
+      item.price = 0
+    }
+     return sum + item.price
+  } ,0)
+  const sum = document.querySelector('.sum > span')
+  sum.textContent = allprice
+}
+
+const deleteCardListItem = (item) => {
+  wishList = wishList.filter((f)=> f.name !== item.name)
+  const allGames = [...document.querySelectorAll('.name')]
+  allGames.forEach((game)=>{
+    if(game.textContent === item.name){
+     game.previousElementSibling.style.color = 'rgb(0,206,255)'
+     game.previousElementSibling.textContent = 'Добавить'
+    }
+  })
+  createCardListItem()
+}
+
+const clearAll = () =>{
+  const clearButton = document.createElement('div')
+  const card = document.querySelector('.card')
+
+  clearButton.classList.add('clear')
+  clearButton.textContent = 'Очистить список'
+  clearButton.addEventListener('click',()=>{
+    const allButtons = [...document.querySelectorAll('.button')]
+
+    wishList = []
+    createCardListItem()
+    allButtons.forEach((game)=>{
+      game.style.color = 'rgb(0,206,255)'
+      game.textContent = 'Добавить'
+      //clearButton.remove()
+    })
+  })
+  card.append(clearButton)
+}
+clearAll()
+
+
