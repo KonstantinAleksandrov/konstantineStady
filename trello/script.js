ef57f6193c045830396eefd67fb47dc3c812a7f9
Greyewi <greyewi@gmail.com>
2022-05-25 18:13:50 +0500

    add trello fixes

--- trello/script.js
+++ trello/script.js
@@ -1,1 +1,37 @@
 'use strict'
+
+const columns = new Map()
+let activeFormName = ""
+
+
+const renderColumnList = (columnList) => { // TODO Отрисовать колонки
+
+}
+
+const renderCardList = () => { // TODO Отрисовать Карточки внутри колонки
+
+}
+
+const handleAddColumn = (name) => {
+  columns.set(name, [])
+  renderColumnList(columns)
+}
+
+const openForm = (target, handler, btnText) => {
+  const defaultForm = document.querySelector('.template__form')
+  const form = defaultForm.cloneNode(true)
+  form.classList.remove('template__form')
+  form.style.display = "block"
+  const formBtn = form.querySelector('.form-footer__add')
+  formBtn.textContent = btnText
+  formBtn.addEventListener('click', () => {
+    handler(activeFormName)
+    activeFormName = ''
+  })
+
+  form.querySelector('textarea').addEventListener('input', (e) => activeFormName = e.target.value )
+  target.after(form)
+}
+
+const addColumnBtn = document.querySelector('.add-column')
+addColumnBtn.addEventListener('click', (e) => openForm(e.target, (e) => handleAddColumn(e), "add column"))
