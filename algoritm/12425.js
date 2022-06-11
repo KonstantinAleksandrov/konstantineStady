d61b5092e125071b9b4a208d341256b59021d1da
Greyewi <greyewi@gmail.com>
2022-06-11 17:34:50 +0500

    fix

--- algoritm/12425.js
+++ algoritm/12425.js
@@ -1,4 +1,4 @@
-const search = (item,arr) =>{
+const search = (item,arr) => {
     let low = 0
     let hight = arr.length - 1
     while(low <= hight){
@@ -15,3 +15,23 @@ const search = (item,arr) =>{
     }
   }
   console.log(search(10,[1,2,3,4,5,6,7,8,9,10]))
+
+
+
+
+//?email=greyewi%40gmail.com&name=%D0%A1%D0%B5%D1%80%D0%B3%D0%B5%D0%B9&age=5
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://gorest.co.in/public/v2/users',
+  headers: { }
+};
+
+axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
