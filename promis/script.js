fb92b12caa7093728e9519fbc4bef4a07705ee6b
KonstantinAleksandrov <constantine22811@gmail.com>
2022-06-26 18:04:17 +0300

    123

--- promis/script.js
+++ promis/script.js
@@ -64,12 +64,7 @@ function getTag(title) {
 
 function getTagItemsAsync(tagId, cb) {
   setTimeout(() => {
-    const tagItems = new Map(JSON.parse([
-    [17,[{"title":"Телепорт бытовой VZHIH-101","price":10000,"discount":7,"available":3},
-    {"title":"Ховерборд Mattel 2016","price":9200,"discount":4,"available":14}]],
-    [32,[{"title":"Ховерборд Mattel 2016","price":9200,"discount":4,"available":14},
-    {"title":"Меч световой FORCE (синий луч)","price":57000,"discount":0,"available":1}]]
-    ]));
+    const tagItems = new Map(JSON.parse('[[17,[{"title":"Телепорт бытовой VZHIH-101","price":10000,"discount":7,"available":3},{"title":"Ховерборд Mattel 2016","price":9200,"discount":4,"available":14}]],[32,[{"title":"Ховерборд Mattel 2016","price":9200,"discount":4,"available":14},{"title":"Меч световой FORCE (синий луч)","price":57000,"discount":0,"available":1}]]]'));
     if (tagItems.has(tagId)) {
       cb(null, tagItems.get(tagId));
     } else {
@@ -91,12 +86,7 @@ function getTagItems(tagId) {
 
 function getCurrencyRateAsync(code, cb) {
   setTimeout(() => {
-    const rates = new Map(JSON.parse([["AUD", 44.95],["AZN", 33.73],["GBP", 73.42],
-    ["AMD", 0.12],["BYN",30.96],["BGN",32.01],["BRL",18.8],["HUF",0.2],["DKK",8.42],
-    ["USD",58.85],["EUR",62.68],["INR",0.88],["KZT",0.18],["CAD",44.74],["KGS",0.85],
-    ["CNY",8.55],["MDL",2.94],["NOK",7.02],["PLN",14.55],["RON",13.92],["ZZZ",79.91],
-    ["SGD",41.36],["TJS",7.43],["TRY",15.97],["TMT",16.84],["UZS",0.02],["UAH",2.16],
-    ["CZK",2.32],["SEK",6.6],["CHF",58.69],["ZAR",4.4],["KRW",0.05],["JPY",0.52]]));
+    const rates = new Map(JSON.parse('[["AUD", 44.95],["AZN", 33.73],["GBP", 73.42],["AMD", 0.12],["BYN",30.96],["BGN",32.01],["BRL",18.8],["HUF",0.2],["DKK",8.42],["USD",58.85],["EUR",62.68],["INR",0.88],["KZT",0.18],["CAD",44.74],["KGS",0.85],["CNY",8.55],["MDL",2.94],["NOK",7.02],["PLN",14.55],["RON",13.92],["ZZZ",79.91],["SGD",41.36],["TJS",7.43],["TRY",15.97],["TMT",16.84],["UZS",0.02],["UAH",2.16],["CZK",2.32],["SEK",6.6],["CHF",58.69],["ZAR",4.4],["KRW",0.05],["JPY",0.52]]'));
     if (!rates.has(code)) {
       return cb(new Error(`Валюта с кодом ${code} не найдена`));
     }
@@ -110,8 +100,3 @@ const tags = [
 ];
 const badTag = { id: 54, title: 'ошибка' };
 const tagTitle = 'гаджеты';
-
-const showTagInfo = (tag) =>{
-
-}
-
